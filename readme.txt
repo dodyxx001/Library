60e603ab15cfb77964cfa31ba68bca54d8b7f5ea
Ziga Dodic <ziga.dodic@gmail.com>
2021-12-21 10:38:23 +0100

    Version 1

---
+++ readme.txt
@@ -0,0 +1,25 @@
+This is a "library" project, which was a part from The Odin 
+Project's curriculum.
+The goal was to practice "Objects" and "Prototypes" part of the 
+Javascript curriculum.
+
+We had to figure out how to design an object template and then create
+new objects (books) with a constructor function.
+We had to write all functions on our own.
+
+In addition of objects, there was a lot of DOM manipulation
+going on when developing this project. And of course CSS and
+HTML writing.
+
+Versions:
+
+Version 1:
+- Wrote constructor function
+- Wrote "create book" function
+- Added functionality to buttons - the trickiest was "remove
+  book" button, because I had to link each button with each 
+  object's index in Library array.
+- Added an "input field" which prompts when we press "add new
+  book" button.
+- Library now accepts new books, and we can delete existing ones
+- Took about 4 hours to complete.
--- script.js
+++ script.js
@@ -1,37 +1,45 @@
+// Array that contains Book objects
 let myLibrary = [];
+
+// Global variables
+const body = document.querySelector('body');
 const content = document.querySelector('content');
+const addNewBookButton = document.querySelector('#add-button');
+const cancelButton = document.querySelector('#cancel');
+const submitButton = document.querySelector('#add-new-book');
+
+const inputForm = document.querySelector('.input-form');
 
 
+
+// Constructor function - creates new Book objects
 function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        if (!read) {
-            return `${title} by ${author},  ${pages} pages , not read yet`;
-        }else{
-            return `${title} by ${author},  ${pages} pages , read`;
-        };
+        return `${title} by ${author},  ${pages} pages , ${read}`;
+        
     };
 };
 
+
+// Function that adds a new book ( 4 args)
 let addBookToLibrary = (title, author, pages, read) => {
     myLibrary.push(new Book(title, author, pages, read));
 };
 
 
-addBookToLibrary ('The Hobbit', 'J.R.R. Tolkien',  295, false);
-
-addBookToLibrary('100miles under the sea', 'jules verne', 300, true);
-
-addBookToLibrary('Da vinci code', 'Dan Brown', 483, true);
-
+// Function that creates html for each book and displays it
 let showBooks = () => {
-    myLibrary.forEach((ele) => {
+    content.innerHTML = "";  // delete current Books html
+    myLibrary.forEach((ele) => { // and for each element 
+
         let newBook = document.createElement('div');
-        newBook.classList.add('book');
-        newBook.innerHTML += `'<div class="info title">
+        newBook.classList.add('book'); 
+        newBook.setAttribute('data-index', `${myLibrary.indexOf(ele)}`); // Numbering the new book
+        newBook.innerHTML += `<div class="info title">  
                                     <p>Title:</p>
                                     <p>${ele.title}</p>
                                 </div>
@@ -44,14 +52,105 @@ let showBooks = () => {
                                     <p id="pages">${ele.pages}</p>
                                 </div>
                                 <div class="read">
-                                    <button>Read yet</button>
+                                    <button class="read-button">${ele.read}</button>
                                 </div>
                                 <div class="remove">
-                                    <button>Remove Book</button>
-                                </div>'`
+                                    <button class="remove-button">Remove Book</button>
+                                </div>`
         content.appendChild(newBook);
-    })
-}
+    });
+};
 
-showBooks();
+
+// Function that enables changing status of "read" to "read" or "not read yet" 
+let readButtonFunctionality = () => {
+    let readButtons = Array.from(document.getElementsByClassName('read-button'));
+    readButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            if (e.target.textContent === 'Read'){
+                e.target.textContent = 'Not read yet';
+            } else {
+                e.target.textContent = 'Read';
+            };
+        });
+    });
+};
+
+
+// Function that enables "remove book" button when called
+let removeBookButtonFunctionality = () => {
+    let allRemoveButtons = Array.from(document.getElementsByClassName('remove-button'));
+
+    allRemoveButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            let index = e.target.parentNode.parentNode.getAttribute('data-index');
+            e.target.parentNode.parentNode.remove()  // Removing HTML
+            myLibrary.splice(index, 1);  // Removing object from array
+
+        });
+    });
+};
+
+
+// Function that removes input form and blur effect
+let removeInputFormAndBlur = () => {
+    inputForm.classList.remove('showing');  // Removing input form
+    body.classList.remove('bodyBlur');
+};
+
+
+// ADD BOOK section
+// Functionality added to Add Book button
+addNewBookButton.addEventListener('click', () => {
+    inputForm.classList.add('showing');  // show the input form
+    body.classList.add('bodyBlur');  // blur the background
+});
+
+
+// Cancel button functionality
+cancelButton.addEventListener('click', () => {
+    removeInputFormAndBlur();
+});
+
+
+// Add button functionality
+submitButton.addEventListener('click', () => {
+    let inputTitle = document.querySelector('#input-title').value
+    let inputAuthor = document.querySelector('#input-author').value
+    let inputPages = document.querySelector('#input-pages').value
+    let inputRead = '';
+        if (document.getElementById("input-read").checked){
+            inputRead = 'Read';
+        } else {
+            inputRead = 'Not read yet';
+    };
+
+    addBookToLibrary(  // Creating new object
+        inputTitle,
+        inputAuthor,
+        inputPages,
+        inputRead
+    );
+
+    removeInputFormAndBlur(); // When the book is added, remove input field
+
+    showBooks(); // Refresh the list
+
+    readButtonFunctionality(); // Adds functionality to "read yet?" buttons
+    
+    removeBookButtonFunctionality(); // Adds func. to "remove book" button
+
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
 
